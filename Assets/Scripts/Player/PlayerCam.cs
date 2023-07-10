2962f26bd0e8a04b810844ba5c4c0266c509d200
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-07-09 19:48:22 -0500

    got movement

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -10,25 +10,42 @@ namespace Coder100.Corridors
         [SerializeField]
         private Transform playerBody;
 
-        private float xRotation = 0;
+        [SerializeField]
+        private float maxX = 90;
+
+        private Quaternion camDefault;
 
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
+            camDefault = transform.localRotation;
         }
 
         private void Update()
         {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+            RotateY();
+            RotateX();
 
-            // y rotation (horiz)
-            playerBody.Rotate(Vector3.up * mouseX);
+            
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+            }
+        }
 
-            // x rotation (vert)
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90, 90);
-            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        private void RotateX()
+        {
+            float horiz = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+            Quaternion rotation = Quaternion.AngleAxis(horiz, Vector3.up);
+            playerBody.localRotation *= rotation;
+        }
+
+        private void RotateY()
+        {
+            float vert = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+            Quaternion rotation = Quaternion.AngleAxis(vert, Vector3.left);
+            Quaternion delta = transform.localRotation * rotation;
+            if (Quaternion.Angle(camDefault, delta) < maxX) transform.localRotation = delta;
         }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,16 +2,50 @@ using UnityEngine;
 
 namespace Coder100.Corridors
 {
-    
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField]
+        private CharacterController controller;
+
+        [SerializeField]
+        private float speed = 12;
+
+        [SerializeField]
+        private float gravity = Physics.gravity.y;
+
+        [SerializeField]
+        private float jumpHeight = 3;
+
+        private Vector3 velocity;
+
         // Update is called once per frame
         private void Update()
         {
+            // gravity
+            if (controller.isGrounded)
+            {
+                if (velocity.y < 0)
+                {
+                    velocity.y = 0;
+                }
+            }
+            else
+            {
+                velocity.y += gravity * Time.deltaTime;
+            }
+
+            // movement
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
-            // Vector3 move = ;
+            Vector3 move = transform.right * x + transform.forward * z + velocity;
+            controller.Move(move * speed * Time.deltaTime);
+
+            // jump
+            if (Input.GetButton("Jump") && controller.isGrounded)
+            {
+                velocity.y = jumpHeight;
+            }
         }
     }
 }
